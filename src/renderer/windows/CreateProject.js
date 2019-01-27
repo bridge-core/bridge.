2748eadb4223dca9cb1fb0a537b2c27c74bbc798
solvedDev <33347616+solvedDev@users.noreply.github.com>
2019-01-27 17:25:32 +0100

    v0.7.0 pt.4
    - improved project creation window

--- src/renderer/windows/CreateProject.js
+++ src/renderer/windows/CreateProject.js
@@ -6,42 +6,14 @@ import { S_IFBLK } from "constants";
 
 export default class CreateFileWindow extends ContentWindow {
     constructor() {
+        const DEFAULT_TEXT = "Projects are stored directly inside the development_behavior_packs folder.";
+
         super({
             display_name: "New Project",
             options: {
                 is_persistent: false,
                 height: 200
             },
-            content: [
-                {
-                    type: "input",
-                    text: "Project Name",
-                    action: {
-                        enter: () => {
-                            this.createProject();
-                        },
-                        default: (val) => {
-                            this.input = val;
-                        }
-                    }
-                },
-                {
-                    type: "input",
-                    text: "Project Description",
-                    action: {
-                        enter: () => {
-                            this.createProject();
-                        },
-                        default: (val) => {
-                            this.des = val;
-                        }
-                    }
-                },
-                {
-                    text: "Projects are stored directly inside the development_behavior_packs folder.",
-                    color: "grey"
-                }
-            ],
             actions: [
                 {
                     type: "space"
@@ -49,14 +21,92 @@ export default class CreateFileWindow extends ContentWindow {
                 {
                     type: "button",
                     text: "Create!",
-                    action: () => this.createProject
+                    action: () => this.createProject()
                 }
             ]
+        });
+
+        this.content = [
+            {
+                type: "input",
+                text: "Project Name",
+                color: "success",
+
+                action: {
+                    enter: () => {
+                        this.createProject();
+                    },
+                    default: (val) => {
+                        if(val == "") {
+                            this.content[0].color = "error";
+                            this.content[2].color = "error";
+                            this.content[2].text = "Please enter a valid project name!";
+                            this.update({
+                                content: this.content
+                            });
+                        } else {
+                            this.content[0].color = "success";
+                            this.content[2].color = "grey";
+                            this.content[2].text = DEFAULT_TEXT;
+                            this.update({
+                                content: this.content
+                            });
+                        }
+                        this.input = val;
+                    }
+                }
+            },
+            {
+                type: "input",
+                text: "Project Description",
+                color: "success",
+
+                action: {
+                    enter: () => {
+                        this.createProject();
+                    },
+                    default: (val) => {
+                        if(val == "") {
+                            this.content[1].color = "error";
+                            this.content[2].color = "error";
+                            this.content[2].text = "Please enter a valid project description!";
+                            this.update({
+                                content: this.content
+                            });
+                        } else {
+                            this.content[1].color = "success";
+                            this.content[2].color = "grey";
+                            this.content[2].text = DEFAULT_TEXT;
+                            this.update({
+                                content: this.content
+                            });
+                        }
+                        this.des = val;
+                    }
+                }
+            },
+            {
+                text: DEFAULT_TEXT,
+                color: "grey"
+            }
+        ];
+        this.input = "";
+        this.des = "";
+        this.update({
+            content: this.content
         });
     }
 
     createProject() {
-        if(this.input == "") return;
+        if(this.input == "" || this.des == "") {
+            this.content[2].color = "error";
+            this.content[2].text = "Please enter a valid project name and description!";
+            this.update({
+                content: this.content
+            });
+
+            return;
+        }
 
         fs.mkdir(BASE_PATH + this.input, (err) => {
             if(err && err.includes("already exists")) return;
