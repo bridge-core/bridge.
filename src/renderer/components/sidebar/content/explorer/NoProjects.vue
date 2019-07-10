52b90ae3c5bce4263eee7177bc551a8ca02f94cc
solvedDev <solvedDev@gmail.com>
2019-07-10 14:58:07 +0200

    + no behavior packs screen

---
+++ src/renderer/components/sidebar/content/explorer/NoProjects.vue
@@ -0,0 +1,26 @@
+<template>
+    <div
+        style="padding: 4px;"
+        
+    >
+        <v-subheader>No Behavior Packs Detected</v-subheader>
+        <v-divider/>
+        <p>It looks like you do not have any projects inside the "development_behavior_packs" folder yet.</p>
+        
+        <v-btn color="success" @click="createBP">Create A Project</v-btn>
+        <v-divider/>
+    </div>
+</template>
+
+<script>
+    import CreateProjectWindow from '../../../../windows/CreateProject';
+
+    export default {
+        name: "explorer-no-projects",
+        methods: {
+            createBP() {
+                new CreateProjectWindow();
+            }
+        }
+    }
+</script>
