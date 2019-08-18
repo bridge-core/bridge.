32b7d216159908c9d4cdd6ec4895924c778f8021
solvedDev <33347616+solvedDev@users.noreply.github.com>
2019-08-18 20:19:53 +0200

    fix build pipeline

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -12,7 +12,6 @@ Vue.use(VueCodeMirror);
 if (!process.env.IS_WEB) Vue.use(require("vue-electron"));
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 export default new Vue({
   components: { App },
   vuetify: new Vuetify({
