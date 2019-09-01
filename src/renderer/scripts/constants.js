5d93bf9a2829fcc2258f60c3d654bf6106940858
solvedDev <solvedDev@gmail.com>
2019-09-01 20:14:10 +0200

    update
    - added item documentation
    - added item component "minecraft:block"
    - added saturation modifier "max"
    - added action & cooldown type "none"

--- src/renderer/scripts/constants.js
+++ src/renderer/scripts/constants.js
@@ -8,5 +8,5 @@ export const BASE_PATH = path.join(MOJANG_PATH, "development_behavior_packs/");
 export const RP_BASE_PATH = path.join(MOJANG_PATH, "development_resource_packs/");
 export const WEB_APP_DATA = "https://solveddev.github.io/bridge-data/";
 export const WEB_APP_PLUGINS = "https://solveddev.github.io/bridge-plugins/";
-export const DOC_LIST = [ "entities", "blocks", "biomes", "addons", "moLang", "UI", "scripting", "particles", "animations", "timelines", "recipes" ];
+export const DOC_LIST = [ "entities", "Item", "blocks", "biomes", "addons", "moLang", "UI", "scripting", "particles", "animations", "timelines", "recipes" ];
 export const MINECRAFT_VERSIONS = JSON.parse(fs.readFileSync(__static + "\\auto_completions\\versions.json").toString());
