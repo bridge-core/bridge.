7b51790a71c35acb9c103522d8b4d97a34cc3039
solvedDev <solvedDev@gmail.com>
2019-09-01 20:12:33 +0200

    refactored custom syntax code

--- src/renderer/scripts/plugins/bridgeCore/EntityHandler.js
+++ src/renderer/scripts/plugins/bridgeCore/EntityHandler.js
@@ -1,55 +1,44 @@
-import uuid from "uuid/v4";
+import { use, set, uuid } from "../../utilities/useAttr";
 
 function transformEvent(event, component_groups, events) {
+    set(event, "add/component_groups", []);
+
     //SPELL EFFECTS
-    let id = uuid();
-    if(event.add && event.add.spell_effects) {
-        if(!event.add.component_groups) event.add.component_groups = [ id ];
-        else event.add.component_groups.push(id);
-
-        component_groups[id] = {
+    let effect_id = uuid();
+    let add_effects = use(event, "add/spell_effects");
+    if(add_effects !== undefined) {
+        set(event, "add/component_groups", effect_id);
+        set(component_groups, effect_id, {
             "minecraft:spell_effects": {
-                "add_effects": event.add.spell_effects
+                "add_effects": add_effects
             }
-        }
-
-        delete event.add.spell_effects;
+        });
     }
-    if(event.remove && event.remove.spell_effects) {
-        if(!event.add.component_groups) event.add.component_groups = [ id ];
-        else if(!component_groups[id]) event.add.component_groups.push(id);
-
-        if(component_groups[id])
-            component_groups[id]["minecraft:spell_effects"].remove_effects = event.remove.spell_effects;
-        else 
-            component_groups[id] = {
-                "minecraft:spell_effects": {
-                    "remove_effects": event.remove.spell_effects
-                }
+    let remove_effects = use(event, "remove/spell_effects");
+    if(remove_effects !== undefined) {
+        if(add_effects === undefined) set(event, "add/component_groups", effect_id);
+        set(component_groups, effect_id, {
+            "minecraft:spell_effects": {
+                "remove_effects": remove_effects
             }
-
-
-        delete event.remove.spell_effects;
+        });
     }
 
     //GROUP
-    if(event.add && event.add.group) {
-        let group_name = event.add.group.name || uuid();
-        if(!event.add.component_groups) event.add.component_groups = [ group_name ];
-        else event.add.component_groups.push(group_name);
-
-        component_groups[group_name] = event.add.group.components || {};
-
-        delete event.add.group;
+    let g = use(event, "add/group");
+    if(g !== undefined) {
+        let group_name = g.name || uuid();
+        set(event, "add/component_groups", group_name);
+        set(component_groups, group_name, g.components || {});
     }
 }
 
 export default function EntityHandler(data) {
     let e = data["minecraft:entity"];
     if(!e) return;
-    if(!e.components) e.components = {};
-    if(!e.component_groups) e.component_groups = {};
-    if(!e.events) e.events = {};
+    set(e, "component_groups", {});
+    set(e, "events", {});
+    set(e, "components", {});
     let { components, component_groups, events } = e;
 
     for(let e in events) transformEvent(events[e], component_groups, events);
