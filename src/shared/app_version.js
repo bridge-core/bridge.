e027af61b300ed7004067982a6807ee3b8fca0da
solvedDev <solvedDev@gmail.com>
2019-10-14 18:16:44 +0200

    v0.13.12

--- src/shared/app_version.js
+++ src/shared/app_version.js
@@ -1,5 +1,1 @@
-/**
- * Version number is in a dedicated file because it's also used by the main thread. 
- * Otherwise, any changes to the main constant file would completely restart "bridge." (in development mode).
-*/
-export default "v0.13.11";
+export default "v0.13.12";
