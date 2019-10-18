d171269bbbb1d635a126add104557db22d099401
solvedDev <solvedDev@gmail.com>
2019-10-18 19:26:43 +0200

    updated documentation

--- src/renderer/scripts/constants.js
+++ src/renderer/scripts/constants.js
@@ -7,7 +7,7 @@ import Store from "../store/index";
 export const WEB_APP_DATA = "https://solveddev.github.io/bridge-data/";
 export const WEB_APP_PLUGINS = "https://solveddev.github.io/bridge-plugins/";
 
-export const DOC_URL = "https://bedrock.dev/1.13.0.0/1.13.0.16/";
+export const DOC_URL = "https://bedrock.dev/1.14.0.0/1.14.0.1/";
 export const DOC_LIST = [ "Entities", "Item", "Blocks", "Biomes", "Addons", "MoLang", "UI", "Scripting", "Particles", "Animations", "Entity Events", "Recipes" ];
 
 export const MINECRAFT_VERSIONS = JSON.parse(fs.readFileSync(path.join(__static, "auto_completions/versions.json")).toString("UTF-8"));
