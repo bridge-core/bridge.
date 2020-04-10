404fcb33b880ddf6a595509a0a475519fa64d71b
solvedDev <solvedDev@gmail.com>
2020-04-10 21:04:04 +0200

    add package backup confirmation

--- src/renderer/scripts/contextMenus/BPMore.ts
+++ src/renderer/scripts/contextMenus/BPMore.ts
@@ -51,63 +51,75 @@ export default [
 		icon: 'mdi-package-variant-closed',
 		title: 'Package Project',
 		action: async () => {
-			new InputWindow(
-				{
-					header: 'Project Name',
-					label: 'Name',
-					text: '',
-				},
-				async project_name => {
-					//Make sure that the resource pack can be loaded
-					if (!CURRENT.RESOURCE_PACK)
-						return new InformationWindow(
-							'No Resource Pack',
-							'Please connect a resource pack before packaging the whole project.'
-						)
+			new ConfirmWindow(
+				() => {
+					new InputWindow(
+						{
+							header: 'Project Name',
+							label: 'Name',
+							text: '',
+						},
+						async project_name => {
+							//Make sure that the resource pack can be loaded
+							if (!CURRENT.RESOURCE_PACK)
+								return new InformationWindow(
+									'No Resource Pack',
+									'Please connect a resource pack before packaging the whole project.'
+								)
 
-					//Package whole project
-					let lw = new LoadingWindow()
-					await fs.mkdir(join(MOJANG_PATH, 'bridge_proj_tmp'), {
-						recursive: true,
-					})
-					await Promise.all([
-						zip(
-							CURRENT.PROJECT_PATH,
-							join(
-								MOJANG_PATH,
-								'bridge_proj_tmp',
-								`${CURRENT.PROJECT}.mcpack`
+							//Package whole project
+							let lw = new LoadingWindow()
+							await fs.mkdir(
+								join(MOJANG_PATH, 'bridge_proj_tmp'),
+								{
+									recursive: true,
+								}
 							)
-						),
-						zip(
-							CURRENT.RP_PATH,
-							join(
-								MOJANG_PATH,
-								'bridge_proj_tmp',
-								`${CURRENT.RESOURCE_PACK}.mcpack`
-							)
-						),
-					])
-					await zip(
-						join(MOJANG_PATH, 'bridge_proj_tmp'),
-						join(MOJANG_PATH, `${project_name}.mcaddon`)
-					)
-					await trash(join(MOJANG_PATH, 'bridge_proj_tmp'))
-					lw.close()
-
-					//Notify user the packaging is complete
-					const ready_push = new Notification({
-						display_icon: 'mdi-package-variant-closed',
-						display_name: 'Package ready!',
-						color: 'info',
-						action: () => {
-							ready_push.remove()
-							remote.shell.showItemInFolder(
+							await Promise.all([
+								zip(
+									CURRENT.PROJECT_PATH,
+									join(
+										MOJANG_PATH,
+										'bridge_proj_tmp',
+										`${CURRENT.PROJECT}.mcpack`
+									)
+								),
+								zip(
+									CURRENT.RP_PATH,
+									join(
+										MOJANG_PATH,
+										'bridge_proj_tmp',
+										`${CURRENT.RESOURCE_PACK}.mcpack`
+									)
+								),
+							])
+							await zip(
+								join(MOJANG_PATH, 'bridge_proj_tmp'),
 								join(MOJANG_PATH, `${project_name}.mcaddon`)
 							)
-						},
-					}).send()
-				}
+							await trash(join(MOJANG_PATH, 'bridge_proj_tmp'))
+							lw.close()
+
+							//Notify user the packaging is complete
+							const ready_push = new Notification({
+								display_icon: 'mdi-package-variant-closed',
+								display_name: 'Package ready!',
+								color: 'info',
+								action: () => {
+									ready_push.remove()
+									remote.shell.showItemInFolder(
+										join(
+											MOJANG_PATH,
+											`${project_name}.mcaddon`
+										)
+									)
+								},
+							}).send()
+						}
+					)
+				},
+				() => {},
+				'Please backup your project before packaging it!'
 			)
 		},
 	},
