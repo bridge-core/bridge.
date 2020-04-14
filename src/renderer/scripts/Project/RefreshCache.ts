c569a407fd12cfe5035fb494579c6b875a21f6ad
solvedDev <solvedDev@gmail.com>
2020-04-14 21:55:14 +0200

    try loading RP before refreshing cache

--- src/renderer/scripts/Project/RefreshCache.ts
+++ src/renderer/scripts/Project/RefreshCache.ts
@@ -3,8 +3,11 @@ import FileSystem from '../FileSystem'
 import JSONTree from '../editor/JsonTree'
 import LightningCache from '../editor/LightningCache'
 import ComponentRegistry from '../plugins/CustomComponents'
+import { trySetRP } from '../Utilities/FindRP'
 
 export async function refreshCache(refresh_rp = false) {
+	if (refresh_rp && !CURRENT.RESOURCE_PACK) await trySetRP() //Try loading the RP if it's not loaded yet
+
 	let explorer = refresh_rp ? CURRENT.RPFileExplorer : CURRENT.BPFileExplorer
 	if (explorer === undefined) return
 
