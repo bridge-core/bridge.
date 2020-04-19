a9964256fb1c6d6fcdb80064317b932032526971
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-04-19 18:01:15 +0200

    added update data check

--- src/renderer/scripts/Utilities/FetchLatestJson.ts
+++ src/renderer/scripts/Utilities/FetchLatestJson.ts
@@ -1,6 +1,7 @@
 import * as VERSION_UTILS from './VersionUtils'
 import { WEB_APP_DATA, APP_VERSION } from '../constants'
 import { isUndefined } from 'util'
+import { platform } from 'os'
 
 // new improved data!
 export interface newVersionRes {
@@ -10,6 +11,7 @@ export interface newVersionRes {
 	downloads?: number
 	latest_version_name?: string
 	urls?: Array<string>
+	download_available?: boolean
 }
 // this ask the github's RESTful api for the bridge.'s latest release data
 export default async function fetchLatestJson() {
@@ -35,9 +37,26 @@ export default async function fetchLatestJson() {
 				APP_VERSION,
 				data.tag_name
 			)
-
-			// Count the downloads + push the download url to the interface
+			// find the correct file extension
+			let extension: String;
+			switch (platform()) {
+				case 'darwin': {
+					extension = '.dmg';
+					break;
+				}
+				case 'win32': {
+					extension = '.exe';
+					break;
+				}
+				case 'linux': {
+					extension = '.AppImage';
+					break;
+				}
+				default: {break;}
+			}
+			// Count the downloads + push the download url to the interface + check if the user's platform download is present
 			for (let asset of data.assets) {
+				if ( asset.name.endsWith(extension) ) res.download_available = true;
 				res.downloads += Number(asset.download_count)
 				res.urls.push(String(asset.browser_download_url))
 			}
