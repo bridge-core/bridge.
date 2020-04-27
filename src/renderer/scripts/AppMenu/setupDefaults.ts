5dfbb41f3168a0b383b00a8626f5d314ab849aeb
solvedDev <solvedDev@gmail.com>
2020-04-27 12:30:49 +0200

    fix keybinding error

--- src/renderer/scripts/AppMenu/setupDefaults.ts
+++ src/renderer/scripts/AppMenu/setupDefaults.ts
@@ -6,13 +6,16 @@ import { ToolMenu } from './Category/tools'
 import { EditMenu } from './Category/edit'
 import { setupKeyBindings } from '../editor/KeyBindings/main'
 import Store from '../../store/index'
+import { IDisposable } from '../Types/disposable'
 
 let CATEGORIES = [FileMenu, EditMenu, ToolMenu, HelpMenu]
+let DISPOSABLES: IDisposable[] = []
 export const DEV_MENU = createAppMenu(DevMenu, false)
 
 export function setupDefaultMenus() {
+	DISPOSABLES.forEach(dis => dis.dispose())
 	setupKeyBindings()
-	CATEGORIES.forEach(c => createAppMenu(c))
+	DISPOSABLES = CATEGORIES.map(c => createAppMenu(c))
 
 	if (Store.state.Settings.is_dev_mode) {
 		DEV_MENU.add()
