56d4c242a7a4753a998c0868e349ce61c4f28989
solvedDev <solvedDev@gmail.com>
2020-04-27 12:40:52 +0200

    + unit tests

---
+++ src/renderer/scripts/Compiler/File/__tests__/OBJtoMC.ts
@@ -0,0 +1,31 @@
+import { OBJtoMC } from '../OBJtoMC'
+
+describe('FileCompiler', () => {
+	test('OBJtoMC - Default', async () => {
+		const {
+			'minecraft:geometry': [{ description, bones }],
+		} = await OBJtoMC(
+			'./src/renderer/scripts/Compiler/File/__tests__/test.obj',
+			'./src/renderer/scripts/Compiler/File/__tests__/test.png',
+			'unknown',
+			1
+		)
+
+		//DESCRIPTION
+		expect(description.identifier).toBe('unknown')
+		expect(description.texture_height).toBe(1)
+		expect(description.texture_width).toBe(1)
+
+		//BONES
+		expect(bones.length).toBe(1)
+		expect(bones[0].poly_mesh).toBeDefined()
+
+		const polyMesh = bones[0].poly_mesh
+		expect(polyMesh.normalized_uvs).toBe(true)
+		expect(polyMesh.positions.length).toBe(4)
+		expect(polyMesh.normals.length).toBe(1)
+		expect(polyMesh.uvs.length).toBe(1)
+		expect(polyMesh.polys.length).toBe(1)
+		expect(polyMesh.polys[0].length).toBe(4)
+	})
+})
