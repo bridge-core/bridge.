068d0868fad725ae47bfd5f0f77ca7a123187ea9
solvedDev <solvedDev@gmail.com>
2020-05-22 00:41:47 +0200

    tweak command parsing

--- app/renderer/src/bridgeCore/functions/parse.ts
+++ app/renderer/src/bridgeCore/functions/parse.ts
@@ -6,37 +6,47 @@ import {
 import LightningCache from '../../editor/LightningCache'
 
 export async function parseFunction(str: string, filePath: string) {
-	const commands = new Set<string>()
-	const lines = str.split('\n').map(l => {
-		for (let [commandName, command] of CommandRegistry) {
-			if (l[0] === '#') continue
-
-			if (l.startsWith(`${commandName}`)) {
-				commands.add(commandName)
-
-				const [_, ...args] = splitCommand(l)
-				const applyData = command.onApply(
-					parseCommandArguments(args.filter(arg => arg !== ''))
-				)
-				return Array.isArray(applyData)
-					? applyData.join('\n')
-					: applyData
-			}
-		}
-
-		const [command, ...args] = splitCommand(l)
-
-		return `${command} ${parseCommandArguments(
-			args.filter(arg => arg !== ''),
-			false
-		).join(' ')}`
-	})
+	const [commands, lines] = parseCommands(str)
 
 	await LightningCache.setPlainData(filePath, {
 		custom_commands: Array.from(commands).concat(Array.from(UsedSelectors)),
 	})
 	UsedSelectors.clear()
 	return lines.join('\n')
+}
+
+export function parseCommands(commands: string) {
+	const usedCommands = new Set<string>()
+
+	return <[Set<string>, string[]]>[
+		usedCommands,
+		commands
+			.split('\n')
+			.map(l => {
+				for (let [commandName, command] of CommandRegistry) {
+					if (l[0] === '#') continue
+
+					if (l.startsWith(`${commandName}`)) {
+						usedCommands.add(commandName)
+
+						const [_, ...args] = splitCommand(l)
+						return command.onApply(
+							parseCommandArguments(
+								args.filter(arg => arg !== '')
+							)
+						)
+					}
+				}
+
+				const [command, ...args] = splitCommand(l)
+
+				return `${command} ${parseCommandArguments(
+					args.filter(arg => arg !== ''),
+					false
+				).join(' ')}`
+			})
+			.flat(Infinity),
+	]
 }
 
 export function splitCommand(command: string) {
