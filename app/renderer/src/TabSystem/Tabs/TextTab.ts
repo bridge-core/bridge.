595b688f3ee3609ac70b98008341d1cccbca938f
solvedDev <solvedDev@gmail.com>
2020-05-29 13:44:33 +0200

    new tab system tweaks

--- app/renderer/src/TabSystem/Tabs/TextTab.ts
+++ app/renderer/src/TabSystem/Tabs/TextTab.ts
@@ -1,21 +1,21 @@
 import { CommonTab } from './Common'
-import { TabSystem } from '../create'
+import { TabSystem } from '../main'
 import { promises as fs } from 'fs'
 
-// export class TextTab extends CommonTab<string> {
-// 	constructor(protected parent: TabSystem, protected filePath: string) {
-// 		super(parent, filePath)
-// 		this.fileContent = fs
-// 			.readFile(filePath)
-// 			.then(buffer => buffer.toString('utf-8'))
-// 			.then(str => (this.fileContent = str))
-// 	}
+export class TextTab extends CommonTab<string> {
+	constructor(protected parent: TabSystem, protected filePath: string) {
+		super(parent, filePath)
+		this.fileContent = fs
+			.readFile(filePath)
+			.then(buffer => buffer.toString('utf-8'))
+			.then(str => (this.fileContent = str))
+	}
 
-// 	async save() {
-// 		await fs.writeFile(this.filePath, this.fileContent)
-// 	}
+	async save() {
+		await fs.writeFile(this.filePath, this.fileContent)
+	}
 
-// 	async saveAs(filePath: string) {
-// 		await fs.writeFile(filePath, this.fileContent)
-// 	}
-// }
+	async saveAs(filePath: string) {
+		await fs.writeFile(filePath, this.fileContent)
+	}
+}
