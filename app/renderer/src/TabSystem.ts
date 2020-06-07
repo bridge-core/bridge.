bridge-core/bridge.

commit 5968af9d26bb1050ef430da43628bfd5ccca0a2b

fixed loading of large files

app/renderer/src/editor/JsonTree.ts
@@ -464,7 +464,10 @@ export default class JSONTree {
 
 		this.addMeta(PROVIDER.getMeta(this.path, file_path, this), file_path)
 
-		if (deep) this.children.forEach(c => c.loadMeta(file_path, true))
+		if (deep)
+			this.children.forEach(c =>
+				requestIdleCallback(() => c.loadMeta(file_path, true))
+			)
 		this.updateUUID()
 	}
 	/**

commit ff8bf54d07bac2da37508da536775c26a2ea2fae

fixed memory leak introduced by 5968af9d26bb1050ef430da43628bfd5ccca0a2b

app/renderer/src/TabSystem.ts
@@ -199,7 +199,9 @@ class TabSystem {
 	//Closing tab
 	internalCloseId(id: number, project = this.project) {
 		//Remove same folder display
-		const { file_path, file_name } = this.projects[this.project][id]
+		const { file_path, file_name, content } = this.projects[this.project][
+			id
+		]
 		for (let i = 0; i < this.projects[this.project].length; i++) {
 			if (id === i) continue
 			const {
@@ -213,6 +215,9 @@ class TabSystem {
 				curr_folders.splice(curr_folders.indexOf(folder_1), 1)
 			}
 		}
+
+		//Dipose JSONTree
+		if (content instanceof JSONTree) content.dispose()
 
 		const [removed] = this.projects[project].splice(id, 1)
 		//Dispose monaco model

app/renderer/src/editor/JsonTree.ts
@@ -17,8 +17,8 @@ import { runValidationFile } from './ScriptRunner/Validation/runFile'
 import { canBeMinified, getCacheData } from './JSONTree/cacheUtils'
 import { trigger } from '../AppCycle/EventSystem'
 
-declare const requestIdleCallback: (func: () => void) => void
-
+declare const requestIdleCallback: (func: () => void) => number
+declare const cancelIdleCallback: (id: number) => void
 let PROVIDER: Provider
 
 export class TreeIterator {
@@ -91,6 +91,7 @@ export default class JSONTree {
 	is_active = true //Whether to output the tree to the final JSON file upon saving
 	uuid: string
 	meta: any
+	cancelCallbacks = new Set<number>()
 	on = {
 		change: new Map<JSONTree, () => void>(),
 		destroy: new Map<JSONTree, () => void>(),
@@ -193,10 +194,18 @@ export default class JSONTree {
 		return false
 	}
 
+	dispose() {
+		this.cancelCallbacks.forEach(id => cancelIdleCallback(id))
+		this.children.forEach(c => c.dispose())
+	}
 	updateUUID() {
 		this.uuid = uuidv4()
 
-		requestIdleCallback(() => this.on.change.forEach(func => func()))
+		const id = requestIdleCallback(() => {
+			this.on.change.forEach(func => func())
+			this.cancelCallbacks.delete(id)
+		})
+		this.cancelCallbacks.add(id)
 	}
 	forEach(cb: (n?: JSONTree) => any) {
 		if (typeof cb !== 'function') return
@@ -465,9 +474,13 @@ export default class JSONTree {
 		this.addMeta(PROVIDER.getMeta(this.path, file_path, this), file_path)
 
 		if (deep)
-			this.children.forEach(c =>
-				requestIdleCallback(() => c.loadMeta(file_path, true))
-			)
+			this.children.forEach(c => {
+				const id = requestIdleCallback(() => {
+					c.loadMeta(file_path, true)
+					this.cancelCallbacks.delete(id)
+				})
+				this.cancelCallbacks.add(id)
+			})
 		this.updateUUID()
 	}
 	/**