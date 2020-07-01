14b53dff3528c8bc321eeab8f78996bbd4a88326
Joelant05 <Joelant05@users.noreply.github.com>
2020-07-01 22:03:36 +0100

    Update WelcomeScreen.vue

--- app/renderer/src/UI/Editor/WelcomeScreen.vue
+++ app/renderer/src/UI/Editor/WelcomeScreen.vue
@@ -1,5 +1,9 @@
 <template>
-	<v-layout :style="`padding: 10em; height: ${available_height}px;`" @dblclick="createFile" column>
+	<v-layout
+		:style="`padding: 10em; height: ${available_height}px;`"
+		@dblclick="createFile"
+		column
+	>
 		<h1>Welcome to bridge.</h1>
 		<div>
 			<br />
@@ -27,11 +31,70 @@
 				</li>
 			</ul>
 		</div>
+		<br />
+		<br />
+		<br />
+		<br />
+		<h2>Need Help?</h2>
+		<br />
+		<div>
+			<v-tooltip bottom color="accent">
+				<template v-slot:activator="{ on, attrs }">
+					<v-btn
+						class="mx-2"
+						fab
+						dark
+						color="#7289DA"
+						@click="openDiscord"
+					>
+						<v-icon dark v-bind="attrs" v-on="on">
+							mdi-discord
+						</v-icon>
+					</v-btn>
+				</template>
+				<span>Join Our Discord Server!</span>
+			</v-tooltip>
+
+			<v-tooltip bottom color="accent">
+				<template v-slot:activator="{ on, attrs }">
+					<v-btn
+						class="mx-2"
+						fab
+						dark
+						color="secondary"
+						@click="openBedrockGuide"
+					>
+						<v-icon dark v-bind="attrs" v-on="on">
+							mdi-book-open-variant
+						</v-icon>
+					</v-btn>
+				</template>
+				<span>Check Out These Addon Guides!</span>
+			</v-tooltip>
+			<v-tooltip bottom color="accent">
+				<template v-slot:activator="{ on, attrs }">
+					<v-btn
+						class="mx-2"
+						fab
+						dark
+						color="primary"
+						@click="openBedrockWiki"
+					>
+						<v-icon dark v-bind="attrs" v-on="on">
+							mdi-earth
+						</v-icon>
+					</v-btn>
+				</template>
+				<span>Check Out This Addon Wiki!</span>
+			</v-tooltip>
+		</div>
 	</v-layout>
 </template>
 
 <script>
 import CreateFileWindow from '../../../windows/CreateFile'
+import DiscordWindow from '../../../windows/Discord'
+import { shell } from 'electron'
 
 export default {
 	name: 'welcome-screen',
@@ -41,6 +104,17 @@ export default {
 	methods: {
 		createFile(event) {
 			new CreateFileWindow(undefined, false)
+		},
+		openDiscord(event) {
+			new DiscordWindow(() => {
+				shell.openExternal('https://discord.gg/jj2PmqU')
+			})
+		},
+		openBedrockGuide(event) {
+			shell.openExternal('https://guide.bedrock.dev')
+		},
+		openBedrockWiki(event) {
+			shell.openExternal('https://wiki.bedrock.dev')
 		},
 	},
 }
