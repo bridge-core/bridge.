473bf1e10077979fe91114866a3faff9350849f3
solvedDev <solvedDev@gmail.com>
2020-07-01 12:34:43 +0200

    format validation files

--- static/validate/entity/groupName.js
+++ static/validate/entity/groupName.js
@@ -1,63 +1,66 @@
-const {
-    Node,
-    GlobalNode,
-    LightningCache,
-    Tab
-} = Bridge
+const { Node, GlobalNode, LightningCache, Tab } = Bridge
 
 let ComponentGroupsNode
 
 const validateGroup = () => {
-    Node.error = undefined
+	Node.error = undefined
 
-    //Validate group data type
-    if (Node.children.length > 0 || Node.data === '') {
-        Node.error = {
-            show: true,
-            message: 'Component group name must be a string',
-            fix: {
-                run: () => {
-                    Node.edit(Node.children[0].key, true)
-                    Node.remove(Node.children[0].key, true)
-                    Tab.setUnsaved()
-                    validateGroup()
-                }
-            }
-        }
-        return
-    }
+	//Validate group data type
+	if (Node.children.length > 0 || Node.data === '') {
+		Node.error = {
+			show: true,
+			message: 'Component group name must be a string',
+			fix: {
+				run: () => {
+					Node.edit(Node.children[0].key, true)
+					Node.remove(Node.children[0].key, true)
+					Tab.setUnsaved()
+					validateGroup()
+				},
+			},
+		}
+		return
+	}
 
-    //Validate group name
-    let componentGroups = []
+	//Validate group name
+	let componentGroups = []
 
-    ComponentGroupsNode = GlobalNode.get('#;bridge_node_skip;#/component_groups')
-    if (ComponentGroupsNode) {
-        ComponentGroupsNode.on.change.set(Node, validateGroup)
-        componentGroups = Object.keys(ComponentGroupsNode.toJSON())
-    }
+	ComponentGroupsNode = GlobalNode.get(
+		'#;bridge_node_skip;#/component_groups'
+	)
+	if (ComponentGroupsNode) {
+		ComponentGroupsNode.on.change.set(Node, validateGroup)
+		componentGroups = Object.keys(ComponentGroupsNode.toJSON())
+	}
 
-    if (!componentGroups.includes(Node.data)) {
-        Node.error = {
-            show: true,
-            message: 'Unknown component group',
-            fix: {
-                run: () => {
-                    GlobalNode.buildFromObject({
-                        [Bridge.FileType === 'entity' ? 'minecraft:entity' : 'bridge:tag']: {
-                            component_groups: {
-                                [Node.data]: {}
-                            }
-                        }
-                    }, undefined, true, true)
+	if (!componentGroups.includes(Node.data)) {
+		Node.error = {
+			show: true,
+			message: 'Unknown component group',
+			fix: {
+				run: () => {
+					GlobalNode.buildFromObject(
+						{
+							[Bridge.FileType === 'entity'
+								? 'minecraft:entity'
+								: 'bridge:tag']: {
+								component_groups: {
+									[Node.data]: {},
+								},
+							},
+						},
+						undefined,
+						true,
+						true
+					)
 
-                    Tab.setUnsaved()
-                    Node.updateUUID()
-                }
-            }
-        }
-    }
+					Tab.setUnsaved()
+					Node.updateUUID()
+				},
+			},
+		}
+	}
 }
-
 
 Node.on.destroy.set(Node, () => ComponentGroupsNode.on.change.delete(Node))
 validateGroup()
