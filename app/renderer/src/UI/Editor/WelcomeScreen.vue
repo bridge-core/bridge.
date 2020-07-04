2f060acd8c04588b9b340b44bae51a46c4d01143
Joelant05 <Joelant05@users.noreply.github.com>
2020-07-04 20:17:42 +0100

    use carousel for cards

--- app/renderer/src/UI/Editor/WelcomeScreen.vue
+++ app/renderer/src/UI/Editor/WelcomeScreen.vue
@@ -11,157 +11,209 @@
 			</v-layout>
 			<v-container>
 				<v-row>
-					<!---Features Card-->
-					<v-card
-						class="mx-auto mb-7"
-						min-width="270"
-						max-width="300"
-						height="420"
-						color="background"
+					<v-carousel
+						continuous
+						hide-delimiters
+						class="mx-16 px-16 pt-6"
 					>
-						<v-card-title class="ml-16 pl-9">Features</v-card-title>
-						<v-expansion-panels accordion flat>
-							<v-expansion-panel>
-								<v-expansion-panel-header color="background">
-									<v-icon small>mdi-auto-fix</v-icon>
-									<span>Syntax highlighting</span>
-								</v-expansion-panel-header>
-								<v-expansion-panel-content color="background">
-									Beautiful syntax highlighting for all files
-								</v-expansion-panel-content>
-							</v-expansion-panel>
-							<v-expansion-panel>
-								<v-expansion-panel-header color="background">
-									<v-icon small
-										>mdi-format-list-bulleted</v-icon
-									>
-									<span>Rich auto-completions</span>
-								</v-expansion-panel-header>
-								<v-expansion-panel-content color="background">
-									We provide hand-written auto-completions for
-									every JSON, lang and mcfunction file!
-								</v-expansion-panel-content>
-							</v-expansion-panel>
-							<v-expansion-panel>
-								<v-expansion-panel-header color="background">
-									<v-icon small>mdi-folder-multiple</v-icon>
-									<span>Easy project mangement</span>
-								</v-expansion-panel-header>
-								<v-expansion-panel-content color="background">
-									Creating addons is easy with bridge.. You
-									can create new behaviour and resource packs
-									with an automatically generated manifest and
-									when your done you can package your project
-									with just one click!
-								</v-expansion-panel-content>
-							</v-expansion-panel>
-							<v-expansion-panel>
-								<v-expansion-panel-header color="background">
-									<v-icon small>mdi-package-variant</v-icon>
-									<span>Custom components</span>
-								</v-expansion-panel-header>
-								<v-expansion-panel-content color="background">
-									With custom entity components you can write
-									Javascript to make your own components with
-									custom arguments.
-								</v-expansion-panel-content>
-							</v-expansion-panel>
-							<v-expansion-panel>
-								<v-expansion-panel-header color="background">
-									<v-icon small>mdi-console-line</v-icon>
-									<span>Custom commands</span>
-								</v-expansion-panel-header>
-								<v-expansion-panel-content color="background">
-									With custom commands you can write
-									Javascript to make custom commands and
-									target selctors that can be used in
-									.mcfunction files.
-								</v-expansion-panel-content>
-							</v-expansion-panel>
-							<v-expansion-panel>
-								<v-expansion-panel-header color="background">
-									<v-icon small>mdi-cog</v-icon>
-									<span>Collection of settings</span>
-								</v-expansion-panel-header>
-								<v-expansion-panel-content color="background">
-									With bridge., you get to choose from a
-									collection of settings that can affect the
-									look of bridge., the format of JSON files,
-									and much more!
-								</v-expansion-panel-content>
-							</v-expansion-panel>
-							<v-expansion-panel>
-								<v-expansion-panel-header color="background">
-									<v-icon small>mdi-cube-outline</v-icon>
-									<span>OBJ model converter</span>
-								</v-expansion-panel-header>
-								<v-expansion-panel-content color="background">
-									The OBJ to JSON model converter can be used
-									to easily convert OBJ models to be used in
-									Minecraft.
-								</v-expansion-panel-content>
-							</v-expansion-panel>
-						</v-expansion-panels>
-					</v-card>
-					<!---Customize Card-->
-					<v-card
-						class="mx-auto mb-7"
-						min-width="250"
-						max-width="300"
-						height="420"
-						color="background"
-					>
-						<v-card-title class="ml-3"
-							>Customize Your Experience</v-card-title
-						>
-						<br />
-						<p>
-							With Plugins You Can Customize Your Experience With
-							bridge.!
-						</p>
-						<ul class="pr-3">
-							<title>What you can do:</title>
-							<br />
-							<header>You can create new...</header>
-							<br />
-							<li>
-								<v-icon small>mdi-palette</v-icon>
-								<span>Themes</span>
-							</li>
-							<li>
-								<v-icon small>mdi-attachment</v-icon>
-								<span>Snippets</span>
-							</li>
-							<li>
-								<v-icon small
-									>mdi-text-box-multiple-outline</v-icon
-								>
-								<span>Presets</span>
-							</li>
-							<li>
-								<v-icon small>mdi-menu</v-icon>
-								<span>UI Elements</span>
-							</li>
-							<li>
-								<v-icon small
-									>mdi-format-list-bulleted-square</v-icon
-								>
-								<span>And Much More!</span>
-							</li>
-						</ul>
-						<p class="pt-10">
-							<v-btn
-								rounded
-								large
-								dark
-								color="primary"
-								@click="openExtensionBrowser"
+						<!---Features Card-->
+						<v-carousel-item>
+							<v-card
+								class="mx-auto mb-7"
+								min-width="270"
+								max-width="300"
+								height="420"
+								color="background"
 							>
-								<v-icon>mdi-puzzle</v-icon>
-								<span>Open Extension Store</span>
-							</v-btn>
-						</p>
-					</v-card>
+								<v-card-title class="ml-16 pl-9"
+									>Features</v-card-title
+								>
+								<v-expansion-panels accordion flat>
+									<v-expansion-panel>
+										<v-expansion-panel-header
+											color="background"
+										>
+											<v-icon small>mdi-auto-fix</v-icon>
+											<span>Syntax highlighting</span>
+										</v-expansion-panel-header>
+										<v-expansion-panel-content
+											color="background"
+										>
+											Beautiful syntax highlighting for
+											all files
+										</v-expansion-panel-content>
+									</v-expansion-panel>
+									<v-expansion-panel>
+										<v-expansion-panel-header
+											color="background"
+										>
+											<v-icon small
+												>mdi-format-list-bulleted</v-icon
+											>
+											<span>Rich auto-completions</span>
+										</v-expansion-panel-header>
+										<v-expansion-panel-content
+											color="background"
+										>
+											We provide hand-written
+											auto-completions for every JSON,
+											lang and mcfunction file!
+										</v-expansion-panel-content>
+									</v-expansion-panel>
+									<v-expansion-panel>
+										<v-expansion-panel-header
+											color="background"
+										>
+											<v-icon small
+												>mdi-folder-multiple</v-icon
+											>
+											<span>Easy project mangement</span>
+										</v-expansion-panel-header>
+										<v-expansion-panel-content
+											color="background"
+										>
+											Creating addons is easy with
+											bridge.. You can create new
+											behaviour and resource packs with an
+											automatically generated manifest and
+											when your done you can package your
+											project with just one click!
+										</v-expansion-panel-content>
+									</v-expansion-panel>
+									<v-expansion-panel>
+										<v-expansion-panel-header
+											color="background"
+										>
+											<v-icon small
+												>mdi-package-variant</v-icon
+											>
+											<span>Custom components</span>
+										</v-expansion-panel-header>
+										<v-expansion-panel-content
+											color="background"
+										>
+											With custom entity components you
+											can write Javascript to make your
+											own components with custom
+											arguments.
+										</v-expansion-panel-content>
+									</v-expansion-panel>
+									<v-expansion-panel>
+										<v-expansion-panel-header
+											color="background"
+										>
+											<v-icon small
+												>mdi-console-line</v-icon
+											>
+											<span>Custom commands</span>
+										</v-expansion-panel-header>
+										<v-expansion-panel-content
+											color="background"
+										>
+											With custom commands you can write
+											Javascript to make custom commands
+											and target selctors that can be used
+											in .mcfunction files.
+										</v-expansion-panel-content>
+									</v-expansion-panel>
+									<v-expansion-panel>
+										<v-expansion-panel-header
+											color="background"
+										>
+											<v-icon small>mdi-cog</v-icon>
+											<span>Collection of settings</span>
+										</v-expansion-panel-header>
+										<v-expansion-panel-content
+											color="background"
+										>
+											With bridge., you get to choose from
+											a collection of settings that can
+											affect the look of bridge., the
+											format of JSON files, and much more!
+										</v-expansion-panel-content>
+									</v-expansion-panel>
+									<v-expansion-panel>
+										<v-expansion-panel-header
+											color="background"
+										>
+											<v-icon small
+												>mdi-cube-outline</v-icon
+											>
+											<span>OBJ model converter</span>
+										</v-expansion-panel-header>
+										<v-expansion-panel-content
+											color="background"
+										>
+											The OBJ to JSON model converter can
+											be used to easily convert OBJ models
+											to be used in Minecraft.
+										</v-expansion-panel-content>
+									</v-expansion-panel>
+								</v-expansion-panels>
+							</v-card>
+						</v-carousel-item>
+						<!---Customize Card-->
+						<v-carousel-item>
+							<v-card
+								class="mx-auto mb-7"
+								min-width="250"
+								max-width="300"
+								height="420"
+								color="background"
+							>
+								<v-card-title class="ml-3"
+									>Customize Your Experience</v-card-title
+								>
+								<br />
+								<p>
+									With Plugins You Can Customize Your
+									Experience With bridge.!
+								</p>
+								<ul class="pr-3">
+									<title>What you can do:</title>
+									<br />
+									<header>You can create new...</header>
+									<br />
+									<li>
+										<v-icon small>mdi-palette</v-icon>
+										<span>Themes</span>
+									</li>
+									<li>
+										<v-icon small>mdi-attachment</v-icon>
+										<span>Snippets</span>
+									</li>
+									<li>
+										<v-icon small
+											>mdi-text-box-multiple-outline</v-icon
+										>
+										<span>Presets</span>
+									</li>
+									<li>
+										<v-icon small>mdi-menu</v-icon>
+										<span>UI Elements</span>
+									</li>
+									<li>
+										<v-icon small
+											>mdi-format-list-bulleted-square</v-icon
+										>
+										<span>And Much More!</span>
+									</li>
+								</ul>
+								<p class="pt-10">
+									<v-btn
+										rounded
+										large
+										dark
+										color="primary"
+										@click="openExtensionBrowser"
+									>
+										<v-icon>mdi-puzzle</v-icon>
+										<span>Open Extension Store</span>
+									</v-btn>
+								</p>
+							</v-card>
+						</v-carousel-item>
+					</v-carousel>
 				</v-row>
 				<v-row>
 					<!---Need Help? Row-->
