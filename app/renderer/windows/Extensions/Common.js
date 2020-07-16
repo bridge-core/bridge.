e42f5b15f685d99fb641f006097d68ccdfea53fb
solvedDev <solvedDev@gmail.com>
2020-07-16 15:03:12 +0200

    WIP: remove need to reload all plugins upon installation/activation/deactivation

--- app/renderer/windows/Extensions/Common.js
+++ app/renderer/windows/Extensions/Common.js
@@ -1,47 +1,15 @@
-import {
-	WEB_APP_PLUGINS,
-	CURRENT
-} from '../../src/constants'
-import {
-	readJSONSync
-} from '../../src/Utilities/JsonFS'
-import {
-	promises as fs
-} from 'fs'
+import { WEB_APP_PLUGINS, CURRENT } from '../../src/constants'
+import { readJSONSync } from '../../src/Utilities/JsonFS'
+import { promises as fs } from 'fs'
 import path from 'path'
 import PluginLoader from '../../src/plugins/PluginLoader'
-import LoadingWindow from '../LoadingWindow'
 import EventBus from '../../src/EventBus'
-import {
-	createNotification
-} from '../../src/UI/Footer/create'
-import {
-	DATA_PATH
-} from '../../../shared/DefaultDir'
+import { DATA_PATH } from '../../../shared/DefaultDir'
+import Store from '../../store/index'
 
 export const EXT_TAG_MAP = readJSONSync(
 	path.join(__static, 'data/ext_tag_map.json')
 )
-/**
- * @type {import('../../src/Types/disposable').IDisposable}
- */
-export let RELOAD_NOTIFICATION
-export const createReloadPush = () => {
-	if (RELOAD_NOTIFICATION !== undefined) return
-
-	RELOAD_NOTIFICATION = createNotification({
-		icon: 'mdi-refresh',
-		message: 'Reload Plugins!',
-		onClick: async () => {
-			RELOAD_NOTIFICATION.dispose()
-			RELOAD_NOTIFICATION = undefined
-
-			let lw = new LoadingWindow().show()
-			await PluginLoader.loadPlugins()
-			lw.close()
-		},
-	})
-}
 
 export function tag(tag_name, index) {
 	return EXT_TAG_MAP[tag_name] || EXT_TAG_MAP[`${index}`] || {}
@@ -50,36 +18,49 @@ export async function download(url, isGlobal = false, pluginPath) {
 	await fetch(WEB_APP_PLUGINS + url)
 		.then(data => data.arrayBuffer())
 		.then(async data => {
-			console.log(DATA_PATH, CURRENT.PROJECT_PATH, isGlobal)
-			if (isGlobal == false) {
+			const basePath = isGlobal
+				? DATA_PATH
+				: path.join(CURRENT.PROJECT_PATH, 'bridge')
+			const pluginFolder = pluginPath
+				? path.basename(`${pluginPath}.zip`)
+				: path.basename(url)
+
+			if (!isGlobal) {
 				await fs.mkdir(
-					path.join(CURRENT.PROJECT_PATH, 'bridge/plugins'), {
+					path.join(CURRENT.PROJECT_PATH, 'bridge/plugins'),
+					{
 						recursive: true,
 					}
 				)
 				await fs.writeFile(
-					pluginPath ? `${pluginPath}.zip` :
-					path.join(
-						CURRENT.PROJECT_PATH,
-						'bridge/plugins',
-						path.basename(url)
-					),
+					pluginPath
+						? `${pluginPath}.zip`
+						: path.join(
+								CURRENT.PROJECT_PATH,
+								'bridge/plugins',
+								path.basename(url)
+						  ),
 					new Buffer(data)
 				)
 			} else {
-				await fs
-					.mkdir(path.join(DATA_PATH, "plugins"))
-					.catch(() => {})
+				await fs.mkdir(path.join(DATA_PATH, 'plugins')).catch(() => {})
 				await fs.writeFile(
-					pluginPath ? `${pluginPath}.zip` :
-					path.join(
-						path.join(DATA_PATH, "plugins"),
-						path.basename(url)
-					),
+					pluginPath
+						? `${pluginPath}.zip`
+						: path.join(
+								path.join(DATA_PATH, 'plugins'),
+								path.basename(url)
+						  ),
 					new Buffer(data)
 				)
 			}
-			createReloadPush()
+
+			await PluginLoader.loadPlugin(basePath, pluginFolder, [])
+
+			Store.commit(
+				'finishedPluginLoading',
+				PluginLoader.getInstalledPlugins()
+			)
 		})
 		.catch(console.error)
 }
@@ -88,13 +69,13 @@ export function getInfoMap() {
 	let res = {}
 
 	for (let {
-			id,
-			version,
-			pluginPath
-		} of PluginLoader.getInstalledPlugins()) {
+		id,
+		version,
+		pluginPath,
+	} of PluginLoader.getInstalledPlugins()) {
 		res[id] = {
 			version,
-			pluginPath
+			pluginPath,
 		}
 	}
 
@@ -103,7 +84,6 @@ export function getInfoMap() {
 
 EventBus.on('bridge:pluginsLoaded', () => {
 	Session.session_installed = {}
-	if (RELOAD_NOTIFICATION) RELOAD_NOTIFICATION.dispose()
 })
 export default class Session {
 	static data
@@ -114,12 +94,7 @@ export default class Session {
 		this.data = await fetch(WEB_APP_PLUGINS + '/plugins.json')
 			.then(raw => raw.json())
 			.then(data =>
-				data.map(({
-					author,
-					version,
-					tags,
-					...other
-				}) => ({
+				data.map(({ author, version, tags, ...other }) => ({
 					author,
 					version,
 					tags: [`v${version}`, author].concat(tags || []),
@@ -130,7 +105,7 @@ export default class Session {
 	}
 
 	static setSessionInstalled(id, version) {
-		this.session_installed[id] = version
+		this.session_installed[id] = { version }
 	}
 
 	static close() {
