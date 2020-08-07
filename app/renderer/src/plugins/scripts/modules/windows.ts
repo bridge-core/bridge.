614012608e1ab6fdd2c5f8d582a23a8d9478b16c
Joelant05 <Joelant05@users.noreply.github.com>
2020-08-07 12:35:58 +0100

    add input window to windows module

--- app/renderer/src/plugins/scripts/modules/windows.ts
+++ app/renderer/src/plugins/scripts/modules/windows.ts
@@ -1,5 +1,8 @@
 import { IModuleConfig } from '../types'
-import { createInformationWindow } from '../../../UI/Windows/Common/CommonDefinitions'
+import {
+	createInformationWindow,
+	createInputWindow,
+} from '../../../UI/Windows/Common/CommonDefinitions'
 import { createWindow } from '../../../UI/Windows/create'
 import { Component as VueComponent } from 'vue'
 
@@ -10,4 +13,9 @@ export const WindowModule = ({}: IModuleConfig) => ({
 	) => createWindow(vueComponent, state),
 	createInformationWindow: (displayName: string, displayContent: string) =>
 		createInformationWindow(displayName, displayContent),
+	createInputWindow: (
+		displayName: string,
+		inputLabel: string,
+		onConfirm: (input: string) => void
+	) => createInputWindow(displayName, inputLabel, onConfirm),
 })
