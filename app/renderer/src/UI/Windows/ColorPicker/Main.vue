18d8acc688df34a29afe080349a3c7646e03d694
Joelant05 <Joelant05@users.noreply.github.com>
2020-08-13 21:23:21 +0100

    update color picker window
    -still needs updating to work properly with themes

---
+++ app/renderer/src/UI/Windows/ColorPicker/Main.vue
@@ -0,0 +1,37 @@
+<template>
+	<BaseWindow
+		v-if="shouldRender"
+		windowTitle="Color"
+		:isVisible="isVisible"
+		:hasMaximizeButton="false"
+		:isFullscreen="false"
+		:width="350"
+		:height="350"
+		@closeWindow="close"
+	>
+		<template #default>
+			<v-color-picker v-model="color" mode="hexa" hide-mode-switch />
+		</template>
+	</BaseWindow>
+</template>
+
+<script>
+import BaseWindow from '../Layout/Base'
+
+export default {
+	name: 'ColorPicker',
+	components: {
+		BaseWindow,
+	},
+	props: ['currentWindow'],
+	data() {
+		return this.currentWindow.getState()
+	},
+	methods: {
+		close() {
+			this.currentWindow.close()
+			this.editColorCb(this.color)
+		},
+	},
+}
+</script>
