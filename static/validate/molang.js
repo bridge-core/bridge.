1f2ff6f2a24ba84561771b252085acc85afc0631
solvedDev <solvedDev@gmail.com>
2020-08-15 00:12:17 +0200

    molang validation: reset previous errors

--- static/validate/molang.js
+++ static/validate/molang.js
@@ -1,9 +1,14 @@
+//Reset previous errors
+Bridge.Node.error = {}
+
+//Empty strings don't need to be validated
 if (Bridge.Node.data === '') return
 
 try {
+	//Try to parse MoLang
 	Bridge.MoLang.parse(Bridge.Node.data)
 } catch (err) {
-	console.log(err)
+	//An error occurred -> MoLang is invalid -> Report error
 	Bridge.Node.error = {
 		show: true,
 		message: err.message,
