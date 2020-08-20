4542249c4a877a109d18fe94b7c304af9947fe4f
solvedDev <solvedDev@gmail.com>
2020-08-20 20:30:34 +0200

    fix error upon saving file without bridge folder

--- app/renderer/src/editor/JSONFileMasks.ts
+++ app/renderer/src/editor/JSONFileMasks.ts
@@ -113,6 +113,7 @@ export class JSONFileMasks {
 		this.data = undefined
 	}
 	static async saveMasks() {
+		await fs.mkdir(path.join(CURRENT.PROJECT_PATH, 'bridge'))
 		await writeJSON(
 			path.join(CURRENT.PROJECT_PATH, 'bridge/.file_masks'),
 			this.data,
