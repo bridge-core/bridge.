e7f1020739ad33652cb6f98cac2b9461cbba4e4b
Joelant05 <Joelant05@users.noreply.github.com>
2020-09-02 10:48:38 +0100

    Update the update app window
    now has the refresh option if the update is still rolling out

--- app/renderer/src/UI/Windows/UpdateApp/definition.ts
+++ app/renderer/src/UI/Windows/UpdateApp/definition.ts
@@ -3,14 +3,19 @@ import UpdateAppComponent from './Main.vue'
 import { newVersionRes } from '../../../Utilities/updateApp'
 import { WEB_APP_DATA } from '../../../constants'
 import { Marked } from '@ts-stack/markdown'
+import { IDisposable } from '../../../Types/disposable'
 
-export function createUpdateAppWindow({
-	description,
-	latest_version,
-	downloads,
-	latest_version_name,
-	urls,
-}: newVersionRes) {
+export function createUpdateAppWindow(
+	{
+		description,
+		latest_version,
+		downloads,
+		latest_version_name,
+		download_available,
+		urls,
+	}: newVersionRes,
+	notification: IDisposable
+) {
 	// Check if there's an update name, if not, add a generic "update available"
 	if (!latest_version_name.includes('-')) {
 		latest_version_name = latest_version.concat(' - Update Available')
@@ -23,6 +28,8 @@ export function createUpdateAppWindow({
 		imgSrc: WEB_APP_DATA + 'update_splash.png',
 		isFullscreen: false,
 		urls: urls,
+		downloadAvailable: download_available,
+		notification: notification,
 	})
 	UpdateApp.open()
 	return UpdateApp
