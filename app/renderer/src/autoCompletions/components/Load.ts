b71f46f151a3de60e3adbd2217896bca8cbb0e4a
solvedDev <solvedDev@gmail.com>
2020-11-23 18:54:49 +0100

    fix $load not working with multiple other instructions
    This should in theory be a huge jump as we now really support all instructions (keyed/unkeyed dynamic_template/versioned_template, load, placeholder). Might introduce other compatibility issues though

--- app/renderer/src/autoCompletions/components/Load.ts
+++ app/renderer/src/autoCompletions/components/Load.ts
@@ -1,6 +1,4 @@
-import { DynamicKeyTemplate } from './DynamicKeyTemplate'
 import Provider from '../Provider'
-import { CONTEXT_DOWN, CONTEXT_UP } from '../Dynamic'
 
 export class Load {
 	static confirm(
@@ -18,22 +16,11 @@ export class Load {
 		current: any
 	): any {
 		let { object } = provider.omegaExpression(current.$load)
-
 		if (object[key] !== undefined)
 			return provider.walk(path_arr, object[key])
-		if (DynamicKeyTemplate.confirm(provider, key, path_arr, object))
-			return DynamicKeyTemplate.process(provider, key, path_arr, object)
 
-		for (let k of Object.keys(object)) {
-			if (k[0] === '$') {
-				for (let i = 0; i < path_arr.length + 1; i++) CONTEXT_UP()
-				let { object: object2, value } = provider.omegaExpression(k)
-				for (let i = 0; i < path_arr.length + 1; i++) CONTEXT_DOWN()
-
-				if (value.includes(key) || object2[key] !== undefined)
-					return provider.walk(path_arr, object[k])
-			}
-		}
+		path_arr.unshift(key)
+		return provider.walk(path_arr, object)
 	}
 	static walk(path_arr: string[], arg1: any): any {
 		throw new Error('Method not implemented.')
